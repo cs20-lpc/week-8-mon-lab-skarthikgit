1fcee9966051685bd826dd0f517e28de20db7875
Karthikeyan Shanmugam <skcodehack@gmail.com>
2026-03-10 21:05:19 -0700

    Push with expected output

--- lab09-main.cpp
+++ lab09-main.cpp
@@ -3,75 +3,68 @@
 #include <string>
 using namespace std;
 
-void testBasicStack() {
-    cout << "\n--- Basic push/peek/pop/clear test ---\n";
-    ArrayStack<int> s(3);
-
-    cout << "Initial stack:\n" << s;
-
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    cout << "After pushing 10, 20, 30:\n" << s;
-    cout << "Peek: " << s.peek() << endl;
-
-    try {
-        s.push(40);
+void fillStacks(ArrayStack<int>& a, ArrayStack<int>& b) {
+    for (int i = 0; i < 10; i++) {
+        cout << i << " ";
+        a.push(i);
+        b.push(i);
     }
-    catch (const string& e) {
-        cout << "Expected exception on push to full stack: " << e << endl;
-    }
-
-    s.pop();
-    cout << "After one pop:\n" << s;
-
-    s.clear();
-    cout << "After clear:\n" << s;
-
-    try {
-        s.peek();
-    }
-    catch (const string& e) {
-        cout << "Expected exception on peek empty stack: " << e << endl;
-    }
+    cout << endl;
 }
 
-template <typename T>
-void testRotate(ArrayStack<T>& s) {
-    cout << "\n--- Rotate test ---\n";
-    cout << "Start:\n" << s;
-
-    s.rotate(Stack<T>::RIGHT);
-    cout << "After rotate RIGHT:\n" << s;
-
-    s.rotate(Stack<T>::LEFT);
-    cout << "After rotate LEFT:\n" << s;
+void popFiveFromB(ArrayStack<int>& b) {
+    for (int i = 0; i < 5; i++) {
+        int topVal = b.peek();
+        cout << "Popping " << topVal << " from stack B" << endl;
+        b.pop();
+    }
 }
 
 int main() {
-    cout << "ArrayStack Driver\n";
+    ArrayStack<int> stackA(10);
+    ArrayStack<int> stackB(10);
 
-    testBasicStack();
+    fillStacks(stackA, stackB);
 
-    ArrayStack<int> r(5);
-    r.push(1);
-    r.push(2);
-    r.push(3);
-    testRotate(r);
+    if (stackA.isFull()) {
+        cout << "Stack A is full!" << endl;
+    }
 
-    cout << "\n--- Copy constructor / assignment test ---\n";
-    ArrayStack<string> a(4);
-    a.push("A");
-    a.push("B");
-    a.push("C");
+    cout << "Stack A length: " << stackA.getLength() << endl;
+    cout << "Stack B length: " << stackB.getLength() << endl;
 
-    ArrayStack<string> b(a); // copy constructor
-    cout << "Original a:\n" << a;
-    cout << "Copied b:\n" << b;
+    try {
+        stackA.push(10);
+    }
+    catch (const string&) {
+        cout << "push: error, stack is full, avoiding overflow" << endl;
+    }
 
-    ArrayStack<string> c(2);
-    c = a; // assignment
-    cout << "Assigned c:\n" << c;
+    popFiveFromB(stackB);
+    cout << "Stack B length: " << stackB.getLength() << endl;
+
+    stackB.clear();
+    if (stackB.isEmpty()) {
+        cout << "Stack B is now empty after being cleared" << endl;
+    }
+
+    try {
+        stackB.pop();
+    }
+    catch (const string&) {
+        cout << "pop: error, stack is empty, avoiding underflow" << endl;
+    }
+
+    try {
+        stackB.peek();
+    }
+    catch (const string&) {
+        cout << "peek: error, stack is empty, cannot access the top" << endl;
+    }
+
+    cout << "Stack A length: " << stackA.getLength() << endl;
+    ArrayStack<int> stackC(stackA);
+    cout << "Stack C length: " << stackC.getLength() << endl;
 
     return 0;
 }
